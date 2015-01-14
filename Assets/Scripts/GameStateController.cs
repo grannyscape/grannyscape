c37555c20b8e2c4f640a0863232eea3361d5831d
grannyscape <grannyscape@gmail.com>
2015-01-14 10:41:42 +0200

    - Tile stuff

---
+++ Assets/Scripts/GameStateController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace grannyscape
+{
+
+	public class GameStateController : MonoBehaviour 
+	{
+		enum State
+		{
+			MainMenu,
+			LevelStart,
+			LevelRunning,
+			LevelEnd,
+			EndScreen,
+		}
+
+		private State m_gameState;
+
+		void Start () 
+		{
+		
+		}
+
+		void Update () 
+		{
+			switch (m_gameState) 
+			{
+			case State.MainMenu:
+				break;
+			case State.LevelStart:
+				break;
+			case State.LevelRunning:
+				break;
+			case State.LevelEnd:
+				break;
+			case State.EndScreen:
+				break;
+			}
+		}
+	}
+
+}
