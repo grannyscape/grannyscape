ccbd6f7e1d1ec5ff4ab6bf6536e7272ef3e22d35
grannyscape <grannyscape@gmail.com>
2015-01-15 16:55:22 +0200

    - Mainmenu version 1

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace grannyscape
+{
+
+	public class MainMenu : MonoBehaviour 
+	{
+		public void StartGame()
+		{
+			//Application.LoadLevel(1);
+		}
+
+		public void Credits()
+		{
+
+		}
+	}
+
+}
